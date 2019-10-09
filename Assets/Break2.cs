9b2f14bc34d8b3f534e89729ab4f1fd4aab20933
marcobeccarini <55263390+marcobeccarini@users.noreply.github.com>
2019-10-08 23:02:33 -0500

    Added B level functionality
    Missing just the cordless's sound

---
+++ Assets/Break2.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Break2 : MonoBehaviour
+{
+    public int minForce;//The Minimum Force Required To Break The Object
+    public GameObject broken;//The Broken Object
+
+    void OnCollisionEnter(Collision col)//When Object Collides
+    {
+        print("The Magnitude Was " + col.relativeVelocity.magnitude.ToString());//Print Out The Magnitude Of The Collision
+        if (col.relativeVelocity.magnitude >= minForce)//If The magnitude Of The Collision Is Equal or Greater Than The Minimum Force Required TO Break It
+        {
+            Shatter();
+        }
+    }
+    void Shatter()
+    {
+        broken.GetComponent<AudioSource>().Play();
+    }
+}
+
+
