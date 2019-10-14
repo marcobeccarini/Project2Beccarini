a684bc4f76ad18f0a47e44f91065ca75769b881d
marcobeccarini <55263390+marcobeccarini@users.noreply.github.com>
2019-10-13 22:38:26 -0500

    added main area and small assets

---
+++ Assets/Spider/SpiderAnim.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAnim : MonoBehaviour
+{
+    
+    public GameObject spider;//The spider
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = spider.GetComponent<Animator>();
+        
+
+    }
+    void OnCollisionEnter(Collision col)//When Object Collides
+    {
+        anim.SetTrigger("isdead");
+    }
+   
+}
+
---
+++ Assets/globe/bounce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bounce : MonoBehaviour
+{
+    public int minForce;//The Minimum Force Required To Break The Object
+    public GameObject bouncer;//The Broken Object
+
+    void OnCollisionEnter(Collision col)//When Object Collides
+    {
+        print("The Magnitude Was " + col.relativeVelocity.magnitude.ToString());//Print Out The Magnitude Of The Collision
+        if (col.relativeVelocity.magnitude >= minForce)//If The magnitude Of The Collision Is Equal or Greater Than The Minimum Force Required TO Break It
+        {
+            Bounce();
+        }
+    }
+    void Bounce()
+    {
+
+        bouncer.GetComponent<AudioSource>().Play();
+
+    }
+}
