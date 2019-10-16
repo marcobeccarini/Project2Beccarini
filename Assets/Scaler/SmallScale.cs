8056d9c403b361f812324d84425c362c8936e757
marcobeccarini <55263390+marcobeccarini@users.noreply.github.com>
2019-10-15 19:39:14 -0500

    Added A level functionality
    Need to change some little things still

---
+++ Assets/Scaler/SmallScale.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallScale : MonoBehaviour
+{
+    public GameObject Scaler;
+   
+
+    private Vector3 shrinkScale = new Vector3(.2f, .2f, .2f);
+
+    void OnCollisionEnter(Collision col)//When Object Collides
+    {
+       
+        Scaler.transform.localScale = shrinkScale;
+      
+    }
+   
+}
